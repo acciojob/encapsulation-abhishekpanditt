dadfb537c74b8febc5fdd6f54c39413d776f16bc
abhishekpanditt <badi.abhi@gmail.com>
2023-03-14 23:26:06 +0530

    Assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
+
+    public static void Main(String [] args){
+
+        RWOnly obj = new RWOnly();
+
+        obj.setName("Abhi");
+
+        String ans = obj.getName();
+        System.out.println(ans);
+    }
   
 }
